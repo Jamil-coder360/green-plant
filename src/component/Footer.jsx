5fa000927ff8dbb611633ff698ebc8b3674391ee
Jamil-coder360 <jamil595nhj@gmail.com>
2026-05-27 22:04:24 +0600

    footer add

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,8 +1,52 @@
+import Image from 'next/legacy/image';
 import React from 'react'
-
+import footer from '../assets/footer.png';
+import { BriefcaseConveyorBelt } from 'lucide-react';
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="py-19"
+        style={{
+          backgroundImage: `url('${footer.src}')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          textAlign: "center",
+        }}
+      >
+        <h3 className="text-white font-lato font-black text-[32px] leading-[140%]">Feel free to contact us</h3>
+
+        <div className="flex items-center justify-center space-x-18 pt-9">
+          
+          <span className="flex items-center justify-center p-4 border border-white rounded-full">
+           <FaInstagram />
+          </span>
+          <span className="flex items-center justify-center p-4 border border-white rounded-full">
+            <FaFacebookF />
+          </span>
+          <span className="flex items-center justify-center p-4 border border-white rounded-full">
+            <FaTwitter />
+          </span>
+        </div>
+        <div className="pt-9">
+          <ul className="flex items-center justify-center gap-15">
+            <li  className="font-railway font-bold text-[16px] leading-[150%]"><a href="#">Home</a></li>
+            <li className="font-railway font-bold text-[16px] leading-[150%]"><a href="#">About Us</a></li>
+            <li  className="font-railway font-bold text-[16px] leading-[150%]"><a href="#">Plants</a></li>
+            <li  className="font-railway font-bold text-[16px] leading-[150%]"><a href="#">Delivery</a></li>
+            <li  className="font-railway font-bold text-[16px] leading-[150%]"><a href="#">Blog</a></li>
+            <li  className="font-railway font-bold text-[16px] leading-[150%]"><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="bg-black text-center">
+
+        <p className="font-railway font-bold text-[16px] leading-[150%] py-2">
+         Copyright © 2024 Lush. All rights reserved. Dennis Nzioki DNX 
+        </p>
+      </div>
+    </footer>
   )
 }
 
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
-import hero from "../assets/hero.png"
+import React from "react";
+import hero from "../assets/hero.png";
+import Image from "next/image";
 // console.log(h);
 const Hero = () => {
-    const BGImage={
-        backgroundImage: `url(${hero.src})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-    }
+  const BGImage = {
+    backgroundImage: `url(${hero.src})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
   return (
     <div style={BGImage} className="h-screen w-full">
-        <div className="flex flex-col items-center justify-center h-full text-center px-4">
-            <h1 className="text-4xl md:text-6xl font-bold text-white mb-6">
-               Nature's Beauty Delivered to You
-            </h1>
-            <p className="text-lg md:text-2xl text-white mb-8 max-w-2xl">
-               Nature's beauty is just a click away with our online flower and plant shop. We offer a wide variety of flowers that will bring a touch of nature to your home!
-            </p>
-            {/* <a
-                href="#contact"
-                className="px-8 py-4 bg-gold text-white rounded-md text-lg font-medium hover:bg-gold/90 transition-all"
-            >
-                Get a Quote
-            </a> */}
-        </div>  
-    </div>
-  )
-}
+      <div className="flex flex-col items-center justify-center h-full text-center px-4">
+        <h1 className="text-4xl md:text-6xl font-bold text-white mb-9.25">
+          Nature's Beauty Delivered to You
+        </h1>
+        <p className="text-lg text-white mb-9.25 max-w-[787px] font-medium ">
+          Nature's beauty is just a click away with our online flower and plant
+          shop. We offer a wide variety of flowers that will bring a touch of
+          nature to your home!
+        </p>
 
-export default Hero
+        <div className="flex space-x-4">
+          <button className="bg-[#285A43] rounded-sm text-white px-12.25 py-3  text-lg font-medium hover:bg-green-600 transition duration-300">
+            Book Now
+          </button>
+          <button className="bg-transparent rounded-sm border border-white text-white px-12.25 py-3  text-lg font-medium hover:bg-green-600 transition duration-300 flex space-x-1">
+            <Image
+              src="/hero-play.svg"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            Watch Video
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
